84e623aad276a314a52330eba04766b34bdf91a7
NehGuk <nurture.tumulus_0n@icloud.com>
2026-03-16 14:19:09 +0100

    Fixes typo

--- src/Nav/Nav.tsx
+++ src/Nav/Nav.tsx
@@ -3,14 +3,12 @@ import "./Nav.css"
 
 function Nav() {
   return (
-    <>
-      <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="booking">Booking</NavLink>
-        <NavLink to="/gallery">Gallery</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
-      </nav>
-    </>
+    <nav>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/booking">Booking</NavLink>
+      <NavLink to="/gallery">Gallery</NavLink>
+      <NavLink to="/contact">Contact</NavLink>
+    </nav>
   )
 }
 
