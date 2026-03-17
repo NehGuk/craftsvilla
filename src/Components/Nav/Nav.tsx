25cb15b08a031ca48dee8485856dde99ef5d7812
NehGuk <nurture.tumulus_0n@icloud.com>
2026-03-17 13:19:30 +0100

    Adds logo to navbar

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -1,9 +1,13 @@
 import { NavLink } from "react-router-dom"
 import "./Nav.css"
+import logo from "../../assets/logo.png"
 
 function Nav() {
   return (
     <nav>
+      <NavLink to="/">
+        <img src={logo} alt="Craftsvilla logo" className="logo_nav" />
+      </NavLink>
       <NavLink to="/">Home</NavLink>
       <NavLink to="/about">About</NavLink>
       <NavLink to="/gallery">Gallery</NavLink>
