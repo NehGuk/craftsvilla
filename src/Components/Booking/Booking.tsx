d0da89561789c9416107ac71c40dd5ac13fed33b
NehGuk <nurture.tumulus_0n@icloud.com>
2026-03-17 12:10:30 +0100

    Adds Teamup calendar iframe

--- src/Components/Booking/Booking.tsx
+++ src/Components/Booking/Booking.tsx
@@ -1,14 +1,57 @@
+import { Link } from "react-router-dom"
+import "./Booking.css"
+
 function Booking() {
   return (
     <>
       <h1>Booking</h1>
-      <p>Big calendar here</p>
-      <p>Booking content here. Calendar, content etc.</p>
-      <p>CTA</p>
-      <p>Booking content here. Calendar, content etc.</p>
-      <p>CTA</p>
-      <p>Booking content here. Calendar, content etc.</p>
-      <p>CTA</p>
+      <section>
+        <iframe
+          className="calendar"
+          src="https://teamup.com/ks65if3v84z3g7waoo?tz=Calendar%20default&showSearch=1&showProfileAndInfo=0&showSidepanel=1&showTitle=0&showViewHeader=0&showAgendaDetails=0&showDateControls=1&showDateRange=1"
+          /* style={{ width: "80%", height: "600px" }} */
+          loading="lazy"
+          title="Calendar"
+        ></iframe>
+      </section>
+      <section>
+        <h2>Mosaikk Kunst</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae commodi
+          rem suscipit fugit veniam, ratione fuga id voluptatibus ullam aliquam
+          quia accusantium quasi voluptatum modi. Repellat rerum dignissimos
+          harum veritatis?
+        </p>
+        <p>Pris: NOK 400</p>
+        <h2>Akvarellmaling</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae commodi
+          rem suscipit fugit veniam, ratione fuga id voluptatibus ullam aliquam
+          quia accusantium quasi voluptatum modi. Repellat rerum dignissimos
+          harum veritatis?
+        </p>
+        <p>Pris: NOK 300</p>
+        <h2>Lerretsmaling</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae commodi
+          rem suscipit fugit veniam, ratione fuga id voluptatibus ullam aliquam
+          quia accusantium quasi voluptatum modi. Repellat rerum dignissimos
+          harum veritatis?
+        </p>
+        <p>Pris: NOK 200</p>
+      </section>
+      <section>
+        <h2>How to sign up</h2>
+        <p>
+          On the calendar above, find the desired session and click "Signup".
+          For personalised sessions, or inquiries regarding larger groups,
+          please get in touch and our team will get back to you in the coming
+          days.
+        </p>
+        <Link to="/contact" className="cta">
+          Get in touch
+        </Link>
+      </section>
     </>
   )
 }
