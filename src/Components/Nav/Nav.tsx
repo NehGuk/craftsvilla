7e03cc2d9845e87c85bcff17226c58d4ad286475
NehGuk <nurture.tumulus_0n@icloud.com>
2026-03-18 14:36:31 +0100

    Makes navbar responsive

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -5,16 +5,17 @@ import logo from "../../assets/logo.png"
 function Nav() {
   return (
     <nav>
-      <div className="nav_white_space"></div>
+      <div className="nav_items_1">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/gallery">Gallery</NavLink>
+      </div>
       <div className="nav_logo">
         <NavLink to="/">
           <img src={logo} alt="Craftsvilla logo" className="logo_nav" />
         </NavLink>
       </div>
-      <div className="nav_items">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/about">About</NavLink>
-        <NavLink to="/gallery">Gallery</NavLink>
+      <div className="nav_items_2">
         <NavLink to="/booking">Booking</NavLink>
         <NavLink to="/contact">Contact</NavLink>
       </div>
