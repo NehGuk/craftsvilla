ddada7ae8b272ae25c48a4130cab9ccc47d71e4a
NehGuk <nurture.tumulus_0n@icloud.com>
2026-03-18 10:05:12 +0100

    Positions navbar with flexbox

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -5,14 +5,19 @@ import logo from "../../assets/logo.png"
 function Nav() {
   return (
     <nav>
-      <NavLink to="/">
-        <img src={logo} alt="Craftsvilla logo" className="logo_nav" />
-      </NavLink>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/about">About</NavLink>
-      <NavLink to="/gallery">Gallery</NavLink>
-      <NavLink to="/booking">Booking</NavLink>
-      <NavLink to="/contact">Contact</NavLink>
+      <div className="nav_white_space"></div>
+      <div className="nav_logo">
+        <NavLink to="/">
+          <img src={logo} alt="Craftsvilla logo" className="logo_nav" />
+        </NavLink>
+      </div>
+      <div className="nav_items">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/gallery">Gallery</NavLink>
+        <NavLink to="/booking">Booking</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+      </div>
     </nav>
   )
 }
