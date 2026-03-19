0acd3071b6b3256f953f6d418fd71a27921cb66f
NehGuk <nurture.tumulus_0n@icloud.com>
2026-03-19 10:26:20 +0100

    Styles how to sign up area on booking

--- src/Components/Booking/Booking.tsx
+++ src/Components/Booking/Booking.tsx
@@ -17,39 +17,50 @@ function Booking() {
         <h2>Mosaikk Kunst</h2>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae commodi
-          rem suscipit fugit veniam, ratione fuga id voluptatibus ullam aliquam
-          quia accusantium quasi voluptatum modi. Repellat rerum dignissimos
-          harum veritatis?
+          rem suscipit fugit veniam, ratione fuga id voluptatibus ullam aliquam.{" "}
+          <Link to="/activities/mosaikkkunst">
+            <span>Read more</span>
+          </Link>
         </p>
-        <p>Pris: NOK 400</p>
+        <p>NOK 400</p>
         <h2>Akvarellmaling</h2>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae commodi
           rem suscipit fugit veniam, ratione fuga id voluptatibus ullam aliquam
-          quia accusantium quasi voluptatum modi. Repellat rerum dignissimos
-          harum veritatis?
+          quia accusantium quasi.{" "}
+          <Link to="/activities/akvarellmaling">
+            <span>Read more</span>
+          </Link>
         </p>
-        <p>Pris: NOK 300</p>
+        <p>NOK 300</p>
         <h2>Lerretsmaling</h2>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae commodi
           rem suscipit fugit veniam, ratione fuga id voluptatibus ullam aliquam
-          quia accusantium quasi voluptatum modi. Repellat rerum dignissimos
-          harum veritatis?
+          quia.{" "}
+          <Link to="/activities/lerretsmaling">
+            <span>Read more</span>
+          </Link>
         </p>
-        <p>Pris: NOK 200</p>
+        <p>NOK 200</p>
       </article>
       <article className="booking_content">
-        <h2>How to sign up</h2>
-        <p>
-          On the calendar above, find the desired session and click "Signup".
-          For personalised sessions, or inquiries regarding larger groups,
-          please get in touch and our team will get back to you in the coming
-          days.
-        </p>
-        <Link to="/contact" className="cta">
-          Get in touch
-        </Link>
+        <div className="signup_info">
+          <div className="signup_info_text">
+            {/* <h2>How to sign up</h2> */}
+            <p>
+              <strong>How to sign up?</strong> On the calendar above, find the
+              desired session and click "Signup". For personalised sessions, or
+              inquiries regarding larger groups, please get in touch and our
+              team will get back to you in the coming days.
+            </p>
+          </div>
+          <div className="cta_container signup_info_cta">
+            <Link to="/contact" className="cta">
+              Get in touch
+            </Link>
+          </div>
+        </div>
       </article>
     </section>
   )
