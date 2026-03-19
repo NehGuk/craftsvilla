8e0d011fb64707a47f9e44a537bc7081228357fc
NehGuk <nurture.tumulus_0n@icloud.com>
2026-03-19 13:30:21 +0100

    Styles lerretsmaling component

--- src/Components/Activities/Lerretsmaling/Lerretsmaling.tsx
+++ src/Components/Activities/Lerretsmaling/Lerretsmaling.tsx
@@ -1,28 +1,42 @@
+import "./Lerretsmaling.css"
 import { Link } from "react-router-dom"
+import lerretsmalingCover from "../../../assets/gallery/leirettsmaling/anne-nygard-HUdZ00yfTXE-unsplash.jpg"
 
 function Lerretsmaling() {
   return (
     <section className="content">
       <h1>Lerretsmaling</h1>
-      <article>
-        <p>
+      <article className="activity_area">
+        <img src={lerretsmalingCover} className="deco_img" />
+        <p className="activity_text">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
           blanditiis dicta? Odio in itaque ducimus sed magnam, sit eligendi
           sapiente enim, cupiditate voluptate laboriosam dolore necessitatibus.
-          Ab alias molestiae cumque. Sett inkluderer: Ramme, A4 størrelse
-          treplate, små fargede steiner, gummi.
+          Ab alias molestiae cumque.
         </p>
-        <ul>
-          <li>Ramme</li>
-          <li>Akvarellpapir med designmal</li>
-          <li>Akvarellfarger</li>
-          <li>Pensler</li>
-        </ul>
+
+        <div className="activity_details">
+          <div className="activity_details__info">
+            <p>Sett inkluderer:</p>
+            <ul>
+              <li>Ramme</li>
+              <li>Akrylpapir med designmal</li>
+              <li>Akrylfarger</li>
+              <li>Pensler</li>
+            </ul>
+          </div>
+          <div className="activity_details__price">
+            <p>
+              Pris: <strong>NOK 300</strong>
+            </p>
+          </div>
+        </div>
       </article>
-      <article>
-        <p>Pris: NOK 200</p>
-      </article>
-      <Link to="/booking">Book now</Link>
+      <div className="cta_container activity_cta">
+        <Link to="/booking" className="cta">
+          Book now
+        </Link>
+      </div>
     </section>
   )
 }
