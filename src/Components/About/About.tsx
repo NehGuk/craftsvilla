700244091280a183cd3007a3877de2b7913acf55
NehGuk <nurture.tumulus_0n@icloud.com>
2026-03-19 09:42:09 +0100

    Styles about page

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -1,18 +1,26 @@
 import "./About.css"
 import { Link } from "react-router-dom"
+import aboutImg from "../../assets/gallery/leirettsmaling/josep-martins-l3-erg8nPRU-unsplash.jpg"
 
 function About() {
   return (
     <section className="content">
-      <h1>About</h1>
-      <p>
-        Craftsvilla har som mål å tilby kreative workshops og aktiviteter som
-        fremmer samhold og avslapning blant ulike grupper som foreldre og barn,
-        venner, kolleger og flere. Målet er å skape et innbydende rom hvor folk
-        i alle aldre kan nyte kvalitetstid sammen i fritiden, lage kunst og
-        utforske kreativiteten sin.
-      </p>
-      <Link to="/booking">Book now</Link>
+      <article>
+        <h1>About</h1>
+        <img src={aboutImg} className="decoImg" />
+        <p className="aboutText">
+          <strong>Craftsvilla</strong> har som mål å tilby kreative workshops og
+          aktiviteter som fremmer samhold og avslapning blant ulike grupper som
+          foreldre og barn, venner, kolleger og flere. Målet er å skape et
+          innbydende rom hvor folk i alle aldre kan nyte kvalitetstid sammen i
+          fritiden, lage kunst og utforske kreativiteten sin.
+        </p>
+        <div className="cta_container">
+          <Link to="/booking" className="cta">
+            Book now
+          </Link>
+        </div>
+      </article>
     </section>
   )
 }
