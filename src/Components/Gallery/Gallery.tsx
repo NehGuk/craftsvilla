af69037e4cedfce92d32043ac3509a60a798f7e2
NehGuk <nurture.tumulus_0n@icloud.com>
2026-03-20 10:30:55 +0100

    Adds a few sample images

--- src/Components/Gallery/Gallery.tsx
+++ src/Components/Gallery/Gallery.tsx
@@ -1,9 +1,29 @@
+import "./Gallery.css"
+import img1 from "../../assets/gallery/akvarellmaling/annie-spratt-sVAXV_PbvLs-unsplash.jpg"
+import img2 from "../../assets/gallery/akvarellmaling/evie-s-MicqqGyDQ6w-unsplash.jpg"
+import img3 from "../../assets/gallery/akvarellmaling/geordanna-cordero-2Qg4y32pdCc-unsplash.jpg"
+import img4 from "../../assets/gallery/akvarellmaling/greg-rosenke-TMXIWZncTZU-unsplash.jpg"
+import img5 from "../../assets/gallery/akvarellmaling/kseniya-lapteva-Dr8jO199ORY-unsplash.jpg"
+import img6 from "../../assets/gallery/akvarellmaling/mona-eendra-vC8wj_Kphak-unsplash.jpg"
+
 function Gallery() {
+  const images = [
+    { src: img1, alt: "Image description" },
+    { src: img2, alt: "Image description" },
+    { src: img3, alt: "Image description" },
+    { src: img4, alt: "Image description" },
+    { src: img5, alt: "Image description" },
+    { src: img6, alt: "Image description" },
+  ]
   return (
-    <section className="content">
+    <section className="gallery">
       <h1>Gallery</h1>
-      <article>
-        <p>Gallery here</p>
+      <article className="gallery_area">
+        {images.map((image) => (
+          <div className="gallery_area_img">
+            <img key={image.src} src={image.src} alt={image.alt} />
+          </div>
+        ))}
       </article>
     </section>
   )
