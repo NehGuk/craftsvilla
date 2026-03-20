ee93ea934c7ab6fc04c33ea9e6a7a9af37c1a9ee
NehGuk <nurture.tumulus_0n@icloud.com>
2026-03-20 11:20:50 +0100

    Adds react-photo-album library for gallery

--- src/Components/Gallery/Gallery.tsx
+++ src/Components/Gallery/Gallery.tsx
@@ -1,29 +1,151 @@
 import "./Gallery.css"
+import { MasonryPhotoAlbum } from "react-photo-album"
+import "react-photo-album/masonry.css"
+
 import img1 from "../../assets/gallery/akvarellmaling/annie-spratt-sVAXV_PbvLs-unsplash.jpg"
 import img2 from "../../assets/gallery/akvarellmaling/evie-s-MicqqGyDQ6w-unsplash.jpg"
 import img3 from "../../assets/gallery/akvarellmaling/geordanna-cordero-2Qg4y32pdCc-unsplash.jpg"
 import img4 from "../../assets/gallery/akvarellmaling/greg-rosenke-TMXIWZncTZU-unsplash.jpg"
 import img5 from "../../assets/gallery/akvarellmaling/kseniya-lapteva-Dr8jO199ORY-unsplash.jpg"
 import img6 from "../../assets/gallery/akvarellmaling/mona-eendra-vC8wj_Kphak-unsplash.jpg"
+import img7 from "../../assets/gallery/mosaikkkunst/anna-hu-m4wgQdEsJZU-unsplash.jpg"
+import img8 from "../../assets/gallery/mosaikkkunst/annie-spratt-Z5VMR-7s4JY-unsplash.jpg"
+import img9 from "../../assets/gallery/mosaikkkunst/annie-spratt-hCzHhu1v0fA-unsplash.jpg"
+import img10 from "../../assets/gallery/mosaikkkunst/colin-watts-HvFckEd3SUg-unsplash.jpg"
+import img11 from "../../assets/gallery/mosaikkkunst/giulia-may-hclMkLbYE_M-unsplash.jpg"
+import img12 from "../../assets/gallery/mosaikkkunst/iryna-muller-nEbUNxYaWQU-unsplash.jpg"
+import img13 from "../../assets/gallery/mosaikkkunst/max-williams-_OoK2W7OPRM-unsplash.jpg"
+import img14 from "../../assets/gallery/leirettsmaling/anne-nygard-HUdZ00yfTXE-unsplash.jpg"
+import img15 from "../../assets/gallery/leirettsmaling/birmingham-museums-trust-4lDX-xTLl3Q-unsplash.jpg"
+import img16 from "../../assets/gallery/leirettsmaling/birmingham-museums-trust-HEEvYhNzpEo-unsplash.jpg"
+import img17 from "../../assets/gallery/leirettsmaling/birmingham-museums-trust-RRn7VvZCbas-unsplash.jpg"
+import img18 from "../../assets/gallery/leirettsmaling/europeana-TjegK_z-0j8-unsplash.jpg"
+import img19 from "../../assets/gallery/leirettsmaling/europeana-a0KlhY8GXPg-unsplash.jpg"
+import img20 from "../../assets/gallery/leirettsmaling/europeana-qs7z-_c3s-Y-unsplash.jpg"
+import img21 from "../../assets/gallery/leirettsmaling/josep-martins-l3-erg8nPRU-unsplash.jpg"
+import img22 from "../../assets/gallery/leirettsmaling/klara-kulikova-kiLdjFm2y14-unsplash.jpg"
 
 function Gallery() {
-  const images = [
-    { src: img1, alt: "Image description" },
-    { src: img2, alt: "Image description" },
-    { src: img3, alt: "Image description" },
-    { src: img4, alt: "Image description" },
-    { src: img5, alt: "Image description" },
-    { src: img6, alt: "Image description" },
+  const photos = [
+    {
+      src: img1,
+      width: 800,
+      height: 500,
+    },
+    {
+      src: img2,
+      width: 800,
+      height: 400,
+    },
+    {
+      src: img3,
+      width: 800,
+      height: 600,
+    },
+    {
+      src: img4,
+      width: 800,
+      height: 650,
+    },
+    {
+      src: img5,
+      width: 800,
+      height: 700,
+    },
+    {
+      src: img6,
+      width: 800,
+      height: 300,
+    },
+    {
+      src: img7,
+      width: 800,
+      height: 800,
+    },
+    {
+      src: img8,
+      width: 800,
+      height: 600,
+    },
+    {
+      src: img9,
+      width: 800,
+      height: 500,
+    },
+    {
+      src: img10,
+      width: 800,
+      height: 700,
+    },
+    {
+      src: img11,
+      width: 800,
+      height: 400,
+    },
+    {
+      src: img12,
+      width: 800,
+      height: 900,
+    },
+    {
+      src: img13,
+      width: 800,
+      height: 300,
+    },
+    {
+      src: img14,
+      width: 800,
+      height: 800,
+    },
+    {
+      src: img15,
+      width: 800,
+      height: 600,
+    },
+    {
+      src: img16,
+      width: 800,
+      height: 700,
+    },
+    {
+      src: img17,
+      width: 800,
+      height: 500,
+    },
+    {
+      src: img18,
+      width: 800,
+      height: 600,
+    },
+    {
+      src: img19,
+      width: 800,
+      height: 700,
+    },
+    {
+      src: img20,
+      width: 800,
+      height: 400,
+    },
+    {
+      src: img21,
+      width: 800,
+      height: 600,
+    },
+    {
+      src: img22,
+      width: 800,
+      height: 600,
+    },
   ]
+  {
+    console.log(img1)
+  }
   return (
     <section className="gallery">
       <h1>Gallery</h1>
       <article className="gallery_area">
-        {images.map((image) => (
-          <div className="gallery_area_img">
-            <img key={image.src} src={image.src} alt={image.alt} />
-          </div>
-        ))}
+        <MasonryPhotoAlbum photos={photos} spacing={0} columns={4} />
       </article>
     </section>
   )
